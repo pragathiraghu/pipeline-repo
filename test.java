756f491f0be63c20d9b11be5da654e50161a882b
pragathiraghu <pragathiraghu34@gmail.com>
2024-12-05 22:52:55 +0530

    Update and rename jenkins file to test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+public class  Test {
+
+        public static void main (string[] args {
+               System.out.println("********************************");
+               System.out.println("hello World");
+               System.out.println("********************************");
+                 
+        }
+}
+
+      
